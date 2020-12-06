7a1397025075e275133c53a0e31871e57ce6194a
Mate Kuna <kunamate66@gmail.com>
2020-12-06 18:07:23 +0100

    save message to mongoDB with model schema

--- server.js
+++ server.js
@@ -8,30 +8,51 @@ const app = express();
 // so both Express and socketIO will be running
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const mongoose = require('mongoose');
+const dbUrl = 'mongodb://admin:admin@ds139197.mlab.com:39197/nodejs_socketio';
 
-let messages = [
-	{name: 'Mate', message: 'Hey'},
-	{name: 'Adam', message: 'Hello there'}
-]
+// schema definition, which is an object with the definitions
+let MessageModel = mongoose.model('Message', {
+	name: String,
+	message: String
+})
 
 app.use(express.static(__dirname));
-let server = http.listen(`${port}`, () => {
-    console.log(`Server is listening on port: ${port}`);
-});
 
 // express cant parse request bodies, need this package
 // we expect json format coming in with the HTTP requests:
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/messages', (req, res) => { 
-	res.send(messages);
+mongoose.connect(dbUrl, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	}, (err) => {
+		console.log('mongo db connected', err);
 })
 
-app.post('/messages', (req, res) => { 
-	messages.push(req.body);
-	io.emit('message', req.body);
-	res.sendStatus(200);
+let server = http.listen(`${port}`, () => {
+    console.log(`Server is listening on port: ${port}`);
+});
+
+app.get('/messages', (req, res) => {
+	MessageModel.find({}, (err, messages) => {	// find all messages
+		res.send(messages);
+	});
+})
+
+app.post('/messages', (req, res) => {
+	// create object based on the mongo Model
+	let message = new MessageModel(req.body);
+
+	message.save( (err) => {
+	    if(err) {
+	    	res.sendStatus(500);
+	    }
+
+			io.emit('message', req.body);
+	    res.sendStatus(200);
+	});
 })
 
 io.on('connection', socket => {
